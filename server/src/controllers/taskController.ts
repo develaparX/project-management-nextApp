2492ad95698fc326d4af7b41043ce9e46f3eb802
arunikaz <srarfian@gmail.com>
2025-03-03 17:41:41 +0700

    handling api on front end using redux toolkit

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -65,3 +65,21 @@ export const createTask = async (req: Request, res: Response): Promise<void> => 
     }
 }
 
+export const updateTaskStatus = async (req: Request, res: Response): Promise<void> => {
+    const { taskId } = req.params;
+    const { status } = req.body
+    try {
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: Number(taskId)
+            },
+            data: {
+                status: status
+            }
+        })
+        res.json(updatedTask)
+    } catch (error: any) {
+        res.status(500).json({ message: `Error updating tasks : ${error.message}` })
+    }
+
+}
