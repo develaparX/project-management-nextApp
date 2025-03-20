129f8492168949c221d02e6698f22c75b52d5125
develaparX <srarfian@gmail.com>
2025-03-21 05:22:56 +0700

    Project TimelineView with Gant-task-react done

--- client/src/app/projects/TimelineView/index.tsx
+++ client/src/app/projects/TimelineView/index.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from "@/app/redux";
 import { useGetTasksQuery } from "@/state/api";
-import { DisplayOption, ViewMode } from "gantt-task-react";
+import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
 import React, { useMemo, useState } from "react";
 
 type Props = {
@@ -24,21 +25,79 @@ const TimelineView = ({ id, setIsModalNewTaskOpen }: Props) => {
     locale: "en-US",
   });
 
-  const ganttTask = useMemo(() => {
-    tasks?.map((task) => ({
-      start: new Date(task.startDate as string),
-      end: new Date(task.dueDate as string),
-      name: task.title,
-      id: `Task-${task.id}`,
-      type: "task" as "TaskType",
-      progress: task.points ? (task.points / 10) * 100 : 0,
+  const ganttTasks = useMemo(() => {
+    return tasks
+      ? tasks.map((task) => ({
+          start: new Date(task.startDate as string),
+          end: new Date(task.dueDate as string),
+          name: task.title,
+          id: `Task-${task.id}`,
+          type: "task" as TaskTypeItems,
+          progress: task.points ? (task.points / 10) * 100 : 0,
+          isDisabled: false,
+        }))
+      : [];
+  }, [tasks]);
+
+  const handleViewModeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setDisplayOptions((prev) => ({
+      ...prev,
+      viewMode: event.target.value as ViewMode,
     }));
-  });
+  };
 
   if (isLoading) return <div>Loading....</div>;
   if (error) return <div>An error occured while fetching tasks</div>;
 
-  return <div>TimelineView</div>;
+  return (
+    <div className="px-4 xl:px-6">
+      <div className="flex flex-wrap items-center justify-between gap-2 py-5">
+        <h1 className="me-2 text-lg font-bold dark:text-white">
+          Project Tasks Timeline
+        </h1>
+        <div className="relative inline-block w-64">
+          <select
+            className="focus:shadow-outline hover:border-gray-5 block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            value={displayOptions.viewMode}
+            onChange={handleViewModeChange}
+          >
+            <option value={ViewMode.Day} className="">
+              Day
+            </option>
+            <option value={ViewMode.Week} className="">
+              Week
+            </option>
+            <option value={ViewMode.Month} className="">
+              Month
+            </option>
+          </select>
+        </div>
+      </div>
+
+      <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
+        <div className="timeline">
+          <Gantt
+            tasks={ganttTasks}
+            {...displayOptions}
+            columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
+            listCellWidth="100px"
+            barBackgroundColor={isDarkMode ? "#101214" : "#aeb8c2"}
+            barBackgroundSelectedColor={isDarkMode ? "#000" : "#9ba1a6"}
+          />
+        </div>
+        <div className="px-4 pb-5 pt-1">
+          <button
+            className="rouded flex items-center bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+            onClick={() => setIsModalNewTaskOpen(true)}
+          >
+            Add New Task
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TimelineView;
